5b84ca44530bca7ed677e1e430085c080a16ae6e
Ivan Piankov <ipyankov@dpkapp.ru>
2023-01-24 20:53:55 +0300

    Add system router

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
+import logging
+
+import inject
 from fastapi import FastAPI
 
+from routers.system_routes import system_router
 
 app = FastAPI()
+app.include_router(system_router)
+
+
+def config(binder: inject.Binder):
+    pass
+
+
+@app.on_event("startup")
+async def startup_event() -> None:
+    inject.configure(config, bind_in_runtime=False)
+
+
+@app.on_event("shutdown")
+async def shutdown_event() -> None:
+    logging.info("Shutting down...")
